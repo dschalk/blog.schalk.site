c95a8be30857c0a1644deffa92fae072bd1c4678
dschalk <z@z.z.net>
2020-11-03 11:09:32 -0500

    Edited index.html in the functional section

--- src/routes/functional/index_2.svelte
+++ src/routes/functional/index_2.svelte
@@ -96,17 +96,16 @@ Introduction
 <div>
 <br>
 <div Blass = bold>
-<p>This website is built on the <a href= "https://sapper.svelte.dev/ ">Sapper</a> framework, an extension of <a href="https://svelte.dev/">Svelte 3.</a> It's hosted on a Digital Ocean droplet which is supported by a Haskell WebSockets server on a separate droplet. </p>
-<p>Svelte modules look a lot like plain, old-fashioned HTML pages, with JavaScript inside of script tags and CSS in style tags. Svelte enhancements of plain vanilla HTML facilitate reactivity and animation. They are few and the official tutorials make it convenient to start using them right away. </p>
-<p> The modules in this presentation are small and isolated from other modules, making them easy to understand and maintain. There are no compelling reasons to avoid mutations and side effects, or to impose explicit type constraints on functions. There's no danger of introducing bugs, as would be the case in larger modules with more than one function reading or modifying state. <p> 
-<p> As global spaces increase in size, it becomes increasingly likely that mutating global variables will cause bugs. Variables that are not safely tucked inside of small or medium-sized functions should be immutable in large global spaces.</p>
-<p> Likewise, functions that manipulate values other than those provided as arguments are dangerous in large global spaces. You might be scrupulously careful, but future maintainers might not understand your code as well as you do. If bugs appear, they might be difficult to trace.</p>
-<p> When the global space is just a small isolated module, what we have come to recognize as "best practices" can be safely ignored. All the power and flexibility of the JavaScript programming language is then at our fingertips, allowing us to write more concise, understandable, and easily maintainable code.   </p>
+<p>This website is built on the <a href="https: //sapper.svelte.dev/">Sapper</a> framework, an extension of <a href="https://svelte.dev/">Svelte 3.</a> It's hosted on a Digital Ocean droplet which is supported by a Haskell WebSockets server on a separate droplet. </p>
+<p>Svelte modules look a lot like plain, old-fashioned HTML pages, with JavaScript inside of script tags, CSS in style tags, and mostly standard HTML. Svelte enhancements of plain vanilla HTML facilitate reactivity and animation. They are few and the official tutorials make it convenient to start using them right away. </p>
+<p> The modules in this presentation are small and isolated from other modules, making them easy to understand and maintain. There are no compelling reasons to avoid mutations and side effects, or to impose explicit type constraints on functions. <p> 
+<p> As global spaces increase in size it becomes increasingly likely that reassigning global variables will eventually lead to bugs. Variables should be tucked safely inside of small or medium-sized functions, or else they should be immutable.</p>
+<p> Likewise, functions that manipulate data that isn't provided as arguments, or that affect data prior to returning, are abominations in large global spaces. You might be scrupulously careful, but future maintainers might not understand your code as well as you do. </p>
+<p>But when the global space is just a small isolated Svelte module, what we have come to recognize as "best practices" can be safely cast aside. All the power and flexibility of the JavaScript programming language will be at your fingertips as you write concise, understandable, and easily maintainable code. Avoid security vulnerabilities, and each and every part of the language will be a "good part".  </p>
 </div>
-<h2>Ersatz Monads</h2>
-<p>"Monad" is rigorously defined in Category Theory, but not in this presentation. Here, a monad is a closure that returns a function that facilitates the composition of values (usualy functions) in ways that can be specified by the monad's definition. The Async Monad receives values from a WebSockets server and sends them to a Web Worker. It receives values from the Web Worker and attaches them to a global object synchronized with the DOM. The Promises Monad processes chains of composed values, mostly asynchronous functions, assigning results to various attributes of a global object that is synchronized with the DOM. </p>
-
-<!--
+<h2>Composition With <a href = "http://blog.schalk.site/objectmonad/">Object Monads</a></h2>
+<p>"Monad" is not a rigorously defined technical term in this presentation. The term is used in reference to composable containers <em>cc</em> with attributes <em>x</em>. The encapsulated value <em>cc.x</em> can be any valid JavaScriptvalue <em>v</em>. When cc operates on a function <em>func</em>, <em>func</em> operates on <em>x</em>. A new monad, <em>cc'</em> is returned with <em>x === func(v)</em> instead of the previous <em>x === v</em>.
+<p>Haskell monads are inspired by Category Theory, but they are not Category Theory monads. The Haskell programming language would need to be a mathematical Category for that to be the case. But I don't call them "ersatz monads", and I don't refer to my JavaScript monads that way either. </p>
 <p>This simple monad constructor returns objects with two-components:</p>
 <pre>{monadObject}</pre>
 <p>This vaguely resembles Haskell monads, where composition relies on the operator ">>=" (called "bind"). Bind has type</p> 
@@ -119,6 +118,7 @@ Introduction
 <pre>{functions}</pre>
 <pre>{mF2}</pre>
 
+
 <h2>Composition With <a href = "http://blog.schalk.site/FunctionMonad/">Function Monads</a></h2>
 
 <p>You cannot write better JavaScript code by mimicking superficial aspects of Haskell - obviously. That's cargo-cult programming. So lets drop the ".bnd" and compose this way:</p>
@@ -127,8 +127,7 @@ Introduction
 <pre>{funcs}</pre>
 <p>All you need to do is define your monad constructors something like this:</p>
 <pre>{monadFunction}</pre>
-<p>Here's a <a href = "http://blog.schalk.site/FunctionMonad/">detailed explanation</a> </p> 
-
+<p>Here's a <a href = "http://blog.schalk.site/FunctionMonad/">detailed explanation</a> </p>
 
 <span  class = "tao">  This pattern is used to define a monad used in fetching pseudo-random numbers from the Haskell WebSockets server and then getting their prime decompositions from a Web Worker (see </span> 
 <a href = "http://blog.schalk.site/Monad2/">Async</a>
@@ -142,16 +141,16 @@ Introduction
 <p>Naming monads facilitates computing with previously returned values, as in this example: </p><pre>{functionChain}</pre>
 <p>Naming also facilitates storing computations for future use. For example:</p>
 <pre>{store}</pre>
--->
+
 
 <h2>Some Thoughts about Functional Programming</h2>
 <p>"Functional programming is an approach to programming based on function calls as the primary programming construct." - Greg Michaelson on Page 3 of <i><b>An Introduction to Functional Programming Through Lambda Calculus.</b></i>
 </p>
 <p>The WebSockets server supporting this site is written in the Haskell programming language. It supports multiplayer games in which groups compete, share a to-do list, and chat among themselves. Any player can join any existing group, and can create new groups. Players can try for points or print out all existing solutions to randomly generated puzzles. The Haskell server routes group scores, todo's, and chat messages exclusively to online group members, generates the puzzles, and reliably does all that is required of it with amazing ease and transparency. The purity of functions and immutability of variables keeps the codebase easy to understand and maintain.</p>
 <p>I say "immutability of variables" because elements in objects of type TMVar are routinely removed and replaced.  When removal and replacement of an element are accomplished with one expression, the process looks a lot like mutating an element in a JavaScript object.  </p>
-<p>TMVars are generally accepted as safe and appropriate for use in atomic transactions; i.e., those that must completely reverse if they fail to complete. Banks that use Haskell for online money transfers don't take chances by trying to force JavaScript to perform atomic transactions. They do, however, use <a href = "http://hackage.haskell.org/package/base-4.14.0.0/docs/Control-Concurrent-MVar.html" >MVars</a>, <a href = "http://hackage.haskell.org/package/stm-2.5.0.0/docs/Control-Concurrent-STM-TVar.html">TVars</a>, and <a href="https://hackage.haskell.org/package/stm-2.5.0.0/docs/Control-Concurrent-STM-TMVar.html">TMVars</a>.</p>
+<p>TMVars are generally accepted as safe and appropriate for use in atomic transactions; i.e., those that must completely reverse if they fail to complete. Banks that use Haskell for online money transfers don't take chances by trying to force JavaScript to perform atomic transactions. They do, however, use <a href = "http://hackage.haskell.org/package/base-4.14.0.0/docs/Control-Concurrent-MVar.html" >MVars</a>MVars, <a href = "http://hackage.haskell.org/package/stm-2.5.0.0/docs/Control-Concurrent-STM-TVar.html">TVars</a>, and <a href="https://hackage.haskell.org/package/stm-2.5.0.0/docs/Control-Concurrent-STM-TMVar.html">TMVars</a>.</p>
 
-<p> I created this site to share my enthusiasm for JavaScript functions, and especially for the recursive closures I call "monads". I began this page explaining why I have no need for most of the trappings of what people these days call "functional programming". </p>
-<p>Trying to make JavaScript functions behave like Haskell functions can be enriching and for some, more entertaining than working crossword puzzles; but when we're developing applications, such projects probably belong on the back burner, or next to the Rubik's cube, mahjong tiles, and Chess pieces in the bottom drawer. </p>
-<p> In the modules that follow, I define functions, experiment with functions, admire the power and possibilities of functions, and generally dwell on functions. If, to you, "functional JavaScript" means "cargo-cult JavaScript" (superstitiously imitating Haskell and similar languages), then call me a heretic. Still, I hope you will at least consider the possibility that in some circumstances, abandoning strict adherence to the so-called "functional paradigm" can greatly improve your JavaScript code. </p>
+<p> I created this site to share my enthusiasm for JavaScript functions, and especially for the recursive closures I call "monads".  The previous paragraphs explain why I have no need for most of the trappings of what people these days call "functional programming". </p>
+<p>Trying to make JavaScript functions behave like Haskell functions can be enriching and more entertaining than working crossword puzzles; but when someone is paying you to develop an application, that fun stuff probably needs to chill in the bottom drawer with the Rubik's cube and Chess set. </p>
+<p>In the modules that follow, I define functions, experiment with functions, admire the power and possibilities of functions, and generally dwell on functions. If, to you, "functional JavaScript" means "cargo-cult JavaScript" (superstitiously imitating Haskell and similar languages), then call me a heretic. Still, I hope you will at least consider the possibility that abandoning strict adherence to the so-called "functional paradigm" can greatly improve your JavaScript code.     </p>
 </div>
