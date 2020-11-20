4b9f7e50c7eab9bce22d7bf29f545eb1eb75bcaa
dschalk <pyschalk@gmail.com>
2020-11-20 14:42:01 -0500

    Fixed mistakes in the Simple Monad module

--- src/routes/functional/Monad0.svelte
+++ src/routes/functional/Monad0.svelte
@@ -4,17 +4,13 @@ import {fade} from "svelte/transition"
 let visible = true;
 
 let monadDisplay = `function Monad (x) {
-  let foo;
-  return foo = function foo (func) {
+  return function foo (func) {
     if (func === "stop") return x
     else  {
       x = func(x);
       return foo;
     }
   };
-}
-
-
 }`
 
 function Monad (x) {
@@ -72,7 +68,7 @@ A SIMPLE LITTLE MONAD
 <p> Monads in the Haskell Programming Language were inspired by Category Theory monads. The "monads" discussed herein are inspired by Haskell monads. Here's the definition of the simple monad described in this module: </p>
 <pre>{monadDisplay}</pre>
 
-<div> In "Monad(6)(v=>v+7)(v=>v*4)(v=>v-10)", the expression "Monad(6)" creates a closure around "x" accessible to the return value, fu(). The expression "foo('stop')" causes foo to return the number held in the closure, 42. </div>
+<div> In "Monad(6)(v=>v+7)(v=>v*4)(v=>v-10)", the expression "Monad(6)" creates a closure around "x" accessible to the return value, foo(). The expression "foo('stop')" causes foo to return the number held in the closure, 42. </div>
 <p> As in Category Theory and the Haskell programming language, the monads described above encapsulate sequences of computations. They do it in a drastically different way, without relying on types. </p>
 
 <p> Below, you can supply num in "Monad(num)(sum(7))(prod(4))(sum(-10))('stop')" where </p>
@@ -89,7 +85,7 @@ const sum = a => b => a+b;
 <pre>
 var m = Monad(3)
 m("stop");  // returns 3
-m(v => v**3)  // Returns run()
+m(v => v**3)  // Returns foo()
 m("stop")  // returns 27</pre>
 
 
